6d03a7cff0dd021da59726b4b30833ccd2668efd
Carly Huang <c85615@gmail.com>
2020-01-24 15:12:45 -0800

    Updates

---
+++ precache-manifest.39de18dbaa2fac45f0b07cfc6fa7bd7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65f5227c7b09e6115e053061d55004ce",
+    "url": "/game-of-life/index.html"
+  },
+  {
+    "revision": "4b6f951dded6cdf03875",
+    "url": "/game-of-life/static/css/main.3057a00e.chunk.css"
+  },
+  {
+    "revision": "540a7b910b5dad3b65fc",
+    "url": "/game-of-life/static/js/2.efd9978a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/game-of-life/static/js/2.efd9978a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4b6f951dded6cdf03875",
+    "url": "/game-of-life/static/js/main.047bc71a.chunk.js"
+  },
+  {
+    "revision": "d9907064f5bf9d005819",
+    "url": "/game-of-life/static/js/runtime-main.be0c55ac.js"
+  }
+]);
